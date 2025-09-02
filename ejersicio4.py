0cf21cb3a021f2673a0d1fe0d2a1356742b9063a
norbeyh <marionorbeyhenao8@gmail.com>
2025-09-02 10:22:49 -0500

    Add files via upload

---
+++ ejersicio4.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns 
+
+# Cargar CSV
+df = pd.read_csv("usuarios2.csv")
+
+# Convertir edad y tiempo sesion a números, y eliminar los no válidos
+df["edad"] = pd.to_numeric(df["edad"], errors="coerce")
+df["tiempo sesion"] = pd.to_numeric(df["tiempo sesion"], errors="coerce")
+df = df.dropna(subset=["edad", "tiempo sesion"])
+
+# Eliminar duplicados
+df = df.drop_duplicates(subset=["nombre","edad","pais","tiempo sesion","estado"])
+
+# Generar colores (tantos como puntos válidos)
+colores = sns.color_palette("pastel", len(df))
+
+# Graficar
+plt.figure(figsize=(8,6))
+plt.scatter(df["edad"], df["tiempo sesion"], c=colores, s=100)
+plt.title("Tiempo de sesión por edad")
+plt.xlabel("Edad")
+plt.ylabel("Tiempo de sesión")
+plt.show()
